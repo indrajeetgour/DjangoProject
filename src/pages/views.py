5feb759183907f5c27e2092f8365a5fb99091743
Indrajeet Gour <igour@slb.com>
2020-04-11 21:33:40 +0530

    first commit to master

---
+++ src/pages/views.py
@@ -0,0 +1,26 @@
+from django.shortcuts import render
+from django.http import HttpResponse
+
+# Create your views here.
+
+
+def home_view(request, *args, **kwargs):
+    print(args, kwargs)
+    print(request)
+    # return HttpResponse("<h1>Hello world</h1>")  # string of html code
+    return render(request, "home.html", {})
+
+
+def contact_view(request, *args, **kwargs):
+    # return HttpResponse("<h1>Contacnt Page<h1/>")
+    return render(request, "contact.html", {})
+
+
+def about_view(request, *args, **kwargs):
+    mu_context = {
+        "my_text": "this is about us",
+        "my_number": 123,
+        "my_list": [1,2,3,4]
+    }
+    # return HttpResponse("<h1>Contacnt Page<h1/>")
+    return render(request, "about.html", mu_context)
